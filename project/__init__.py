594157aed9392fb447c17560e3bce1a44ef7c325
FrancisBaziele <70360886+FrancisBaziele@users.noreply.github.com>
2021-03-21 15:43:35 +0000

    update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,1 +1,8 @@
+//importing flask, make sure flask is installed
+from flask import Flask
+import wtforms
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+
+from project import routes
