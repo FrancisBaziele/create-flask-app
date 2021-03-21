241c752dad9599cd7a0b89a1a0fe0b8907e8c90a
FrancisBaziele <70360886+FrancisBaziele@users.noreply.github.com>
2021-03-21 16:13:39 +0000

    update routes.py

--- project/routes.py
+++ project/routes.py
@@ -1,6 +1,9 @@
 from project import app 
+from flask import render_template
+from project.forms import RegistrationForm
 
 @app.route( '/', methods=['GET', 'POST'])
 @app.route( '/home', methods=['GET', 'POST'])
 def home():
-    return '<h1>Hi there </h1>'
+    return render_template('index.html', title='Home')
+    
